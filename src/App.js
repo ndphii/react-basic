dd719bee5f06b007c620b4d168732b20b9230b53
User Name <nguyenducphi002@gmail.com>
2023-04-18 11:45:37 +0700

    s

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/home";
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Route>
+      <Route path="/" component={Home} exact />
+    </Route>
   );
-}
+};
+
 
 export default App;
+// export default chỉ gọi đc một fun
---
+++ src/screens/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/home_components/header";
+import Footer from "../components/home_components/footer";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
